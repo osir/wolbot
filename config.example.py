33ff97d7f7869e7052b2cba83e197840413c23cb
os <gh@svis.ch>
2018-04-28 07:50:53 +0200

    Edited default csv path

--- config.example.py
+++ config.example.py
@@ -3,7 +3,7 @@
 TOKEN='123456789:abcxdefghijklmnopqrs-tuvwxyzabcdefg'
 
 # Location to store the saved machines
-STORAGE_PATH='/tmp/mac-addresses.csv'
+STORAGE_PATH='/opt/wolbot/mac-addresses.csv'
 
 # Separator to use when printing addresses
 MAC_ADDR_SEPARATOR='-'
